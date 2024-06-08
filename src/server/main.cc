f7807cfc2ee14dcb045d0e856d31525286bbd9d9
TrietMinh23 <npmtriet2k3@gmail.com>
2024-06-08 10:47:10 +0700

    feat: Add server functions for logout, socket, listen, accept, and broadcast

--- src/server/main.cc
+++ src/server/main.cc
@@ -3,4 +3,11 @@
 int main(int argc, char *argv[]) {
   const int port = (argc > 1) ? std::atoi(argv[1]) : 8000;
   const int backlog = (argc > 2) ? std::atoi(argv[2]) : 5;
+
+  const int sockfd = Socket();
+  Bind(sockfd, port);
+  Listen(sockfd, backlog);
+  serverChat(sockfd);
+  close(sockfd);
+  return errno;
 }
